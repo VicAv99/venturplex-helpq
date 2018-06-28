1295cb536e2b44a5c9af990698269caa7b8994da
Victor Avila <victor.avila@venturplex.com>
2018-06-28 14:08:01 -0700

    misc

--- src/app/help-requests/help-requests.component.ts
+++ src/app/help-requests/help-requests.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
 import { Observable } from 'rxjs';
 import { Requests } from '../shared/request';
-import { map, concatAll } from 'rxjs/operators';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-help-requests',

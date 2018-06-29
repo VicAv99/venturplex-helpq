9d6202ece46c6575df776b841e2fc77856e0312e
Victor Avila <victor.avila@venturplex.com>
2018-06-29 12:22:51 -0700

    misc

--- src/app/help-requests/help-requests.component.ts
+++ src/app/help-requests/help-requests.component.ts
@@ -45,7 +45,7 @@ export class HelpRequestsComponent implements OnInit {
       'requester': request.requester,
       'assignee': request.assignee,
       'project': request.project,
-      'name': request.summary,
+      'summary': request.summary,
       'description': request.description,
       'createdAt': timestamp
     });
